5ea3b7a23f258d64d8c3de10668df300c5dae6fd
nicolasteodorescu <108652553+nicolasteodorescu@users.noreply.github.com>
2026-05-12 20:28:14 +0300

    Update bubblesort_teamADN.cpp

--- algorithms/bubblesort_teamADN.cpp
+++ algorithms/bubblesort_teamADN.cpp
@@ -6,8 +6,8 @@
 void bubblesort( std::vector<int>&v, int n ) {
     int i, j, aux;
 
-    for ( i = 0; i < n; i++ )
-        for ( j = i + 1; j < n; j++ )
+    for ( i = 0; i <= n; i++ )
+        for ( j = i + 1; j <= n; j++ )
             if ( v[i] > v[j] ) {
                 aux = v[i];
                 v[i] = v[j];
